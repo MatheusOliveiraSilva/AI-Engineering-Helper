298fbc179a7c92e3dccac227ec617f4987445cc3
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-07 13:49:41 -0300

    fix: memory logic

--- chatbot/nodes/nodes.py
+++ chatbot/nodes/nodes.py
@@ -1,7 +1,7 @@
 from chatbot.states.chatbot_states import ChatbotState
 from chatbot.prompts.prompt_v0 import CHATBOT_PROMPT
 from llm_config.llm_model_config import LLMModelConfig
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import SystemMessage, AIMessage
 from vector_database.hybrid_retrieval import HybridSearchRetriever
 
 class ChatbotsNodes:
@@ -42,8 +42,14 @@ class ChatbotsNodes:
             additional_kwargs={"cache-control": {"type": "ephemeral"}}
         )
 
-        response = self.llm.invoke(
-            [sys_msg] + state["messages"]
-        )
+        response = self.llm.invoke(state["messages"] + [sys_msg])
 
-        return {"response": response.content}
+        thinking = response.content[0]["thinking"] if "thinking" in response.content[0] else ""
+        text = response.content[1]["text"] if "text" in response.content[1] else ""
+
+        # Aqui criamos uma nova lista de mensagens, anexando a resposta do modelo
+        state["messages"] = state["messages"] + [AIMessage(content=thinking), AIMessage(content=text)]
+
+        print(state["messages"])
+        # claude 3.7 response looks like [{thinking:..., text:....}], and we spect a dict in front end, so...
+        return {"thinking": thinking, "text": text}
--- chatbot/states/chatbot_states.py
+++ chatbot/states/chatbot_states.py
@@ -5,6 +5,6 @@ from langgraph.graph.message import add_messages
 from langchain_core.documents import Document
 
 class ChatbotState(TypedDict):
-    messages: Annotated[list, add_messages]
+    messages: List
     context: List[Document]
 
