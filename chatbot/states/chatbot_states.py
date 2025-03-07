2cca34a68ff313b64285907f33159e5b185ac3a5
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-07 15:35:46 -0300

    fix: changed state[message] typing

--- chatbot/states/chatbot_states.py
+++ chatbot/states/chatbot_states.py
@@ -5,6 +5,6 @@ from langgraph.graph.message import add_messages
 from langchain_core.documents import Document
 
 class ChatbotState(TypedDict):
-    messages: List
+    messages: Annotated[list, add_messages]
     context: List[Document]
 
